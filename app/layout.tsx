91971e8e49677a8fd0a3678c8cf657c8bcb00c71
Balint <barna0203@gmail.com>
2026-05-13 12:35:38 +0200

    sitemap és robots hozzáadva

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,47 @@
 import type { Metadata } from 'next'
-import { Outfit, Space_Grotesk } from 'next/font/google' //Inter csere Outfit-re
+import { Outfit, Space_Grotesk } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
-// Ez lesz az alapértelmezett betűtípus
+// Betűtípusok beállítása
 const outfit = Outfit({ 
   subsets: ["latin"],
   variable: '--font-outfit'
 });
 
-// Ez marad a címeknek (H1, H2, H3)
 const spaceGrotesk = Space_Grotesk({ 
   subsets: ["latin"],
   variable: '--font-space-grotesk'
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://stackyburger.hu'),
   title: 'STACKY Smashburgers & more | Nyíregyháza',
   description: 'A legropogósabb szélű smash burgerek Nyíregyházán. Látogass el hozzánk és kóstold meg a legjobb smash burgereket!',
   generator: 'v0.app',
   icons: {
-    icon: "/stacky_logo.svg", // Ez a fő ikon a böngésző fülre
-    apple: "/stacky_logo.svg", // Mobilos "kezdőképernyőhöz" is jó lesz ez egyelőre  
+    icon: [
+      { url: "/stacky_logo.svg" }, // Alapértelmezett
+      { url: "/icon-light.png", media: "(prefers-color-scheme: light)" },
+      { url: "/icon-dark.png", media: "(prefers-color-scheme: dark)" },
+    ],
+    apple: "/apple-icon.png",
+  },
+  openGraph: {
+    title: 'STACKY Smashburgers & more | Nyíregyháza',
+    description: 'A legropogósabb szélű smash burgerek Nyíregyházán!',
+    url: 'https://stackyburger.hu',
+    siteName: 'STACKY',
+    images: [
+      {
+        url: '/opengraph-image.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'STACKY Smashburgers kínálata',
+      },
+    ],
+    locale: 'hu_HU',
+    type: 'website',
   },
 }
 
@@ -32,10 +52,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="hu" className="scroll-smooth bg-background">
-      {/* 
-          A 'font-sans' mostantól az Outfit lesz, 
-          a címeknél pedig majd manuálisan a 'font-heading' osztályt 
-      */}
       <body className={`${outfit.variable} ${spaceGrotesk.variable} font-sans antialiased`}>
         {children}
         {process.env.NODE_ENV === 'production' && <Analytics />}
